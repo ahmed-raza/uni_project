d7c59236b7e9278fa76442e72898bca6e59eefdb
Alex Roye <alex@creativefaze.com>
2017-11-03 12:49:18 +0500

    Actions for delete confirmation and actual deletion

--- app/Http/Controllers/AdsController.php
+++ app/Http/Controllers/AdsController.php
@@ -12,7 +12,6 @@ class AdsController extends Controller
 {
   public function index() {
     $ads = Ad::all();
-
     return view('ads.index', compact('ads'));
   }
   public function show($slug) {
@@ -28,11 +27,20 @@ class AdsController extends Controller
     $images = "";
     $request->request->add(['slug'=>$slug, 'images'=>$images,'category_id'=>$request->input('category')]);
     $ad = Auth::user()->ads()->create($request->all());
-    return redirect(route('ads.show', $ad->id))->with('message','Your ad has been posted.');
+    return redirect(route('ads.show', $ad->slug))->with('message','Your ad has been posted.');
   }
   public function edit($id) {
     $ad = Ad::findOrFail($id);
     $categories = Category::pluck('category', 'id');
     return view('ads.edit', compact('ad','categories'));
+  }
+  public function delete($id) {
+    $ad = Ad::findOrFail($id);
+    return view('partials.delete')->with(['entity_type'=>'ads', 'entity_id'=>$ad->id]);
+  }
+  public function destroy($id) {
+    $ad = Ad::findOrFail($id);
+    $ad->delete();
+    return redirect(route('user.ads'))->with('message', 'Ad deleted.');
   }
 }
