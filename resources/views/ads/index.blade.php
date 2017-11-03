b34796e2c099fd17beaba384c5c7ae4ba8b226ee
Alex Roye <alex@creativefaze.com>
2017-11-03 12:49:37 +0500

    Showing ad by slug instead of id

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -7,7 +7,7 @@
     @foreach($ads as $ad)
       <div class="row">
         <div class="col-lg-12">
-          <h3>{{ Html::link(route('ads.show',$ad->id), $ad->title) }}</h3>
+          <h3>{{ Html::link(route('ads.show',$ad->slug), $ad->title) }}</h3>
           {!! str_limit($ad->description, 200) !!}
         </div>
       </div>
