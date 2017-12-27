fe4bb466cf871bb17e546136775f959215e4bd5d
Alex Roye <alex@creativefaze.com>
2017-12-27 16:04:10 +0500

    Added phone, email and contact_info to fillables

--- app/Ad.php
+++ app/Ad.php
@@ -6,7 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ad extends Model
 {
-  protected $fillable = ['user_id', 'title', 'slug', 'category_id', 'city', 'images', 'description', 'approve'];
+  protected $fillable = [
+    'user_id',
+    'title',
+    'slug',
+    'category_id',
+    'city',
+    'images',
+    'description',
+    'approve',
+    'contact_info',
+    'email',
+    'phone'
+    ];
 
   public function user() {
     return $this->belongsTo('App\User');
