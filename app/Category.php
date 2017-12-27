ff62ddfdc3b636c5f24d217ca54560b2ca1cde55
Alex Roye <alex@creativefaze.com>
2017-12-27 12:14:36 +0500

    Changed attribute name

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-  protected $fillable = ['category'];
+  protected $fillable = ['name'];
 
   public function ads() {
       return $this->hasMany('App\Ad');        
