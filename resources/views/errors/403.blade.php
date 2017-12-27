b45b3ed2000ec333b1f87b70012854c6c13c893d
Alex Roye <alex@creativefaze.com>
2017-12-27 13:01:33 +0500

    Custom error pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,3 @@
+@extends('errors::layout')
+
+@section('message', 'Unauthorized Access')
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,3 @@
+@extends('errors::layout')
+
+@section('message', 'Page you are looking for could not be found.')
