97364037f5102be4fd40cb909691db018dc7e522
Alex Roye <alex@creativefaze.com>
2017-12-27 13:00:22 +0500

    Category store and index methods

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoriesController extends Controller
 {
-    //
+  public function index() {
+    $categories = Category::all();
+    return view('admin.categories.index', compact('categories'));
+  }
+  public function store(Request $request) {
+    Category::create($request->all());
+    return redirect()->back()->with('message', 'Category created.');
+  }
 }
