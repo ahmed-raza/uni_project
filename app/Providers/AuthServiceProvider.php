b55047c19753d78c46ebbc6cf0500941ce7dfe77
Ahmed Raza <raza1778@gmail.com>
2018-02-21 00:18:56 +0500

    Defined update-ad gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        Gate::define('update-ad', function($user, $ad){
+            return $user->id == $ad->user_id || $user->role == 'admin';
+        });
     }
 }
