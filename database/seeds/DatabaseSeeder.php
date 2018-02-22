20934bd4490491514b65322970fac49d8a5e8f8b
Ahmed Raza <raza1778@gmail.com>
2018-02-22 19:50:21 +0500

    Seeding categories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,36 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       DB::table('users')->insert([
-          'name'=>'Raza',
+          'name'=>env('MY_NAME'),
           'email'=>env('MY_EMAIL'),
           'password'=>bcrypt('password'),
           'role'=>'admin'
+        ]);
+      DB::table('categories')->insert([
+          'name'=>'Electronics',
+          'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+      DB::table('categories')->insert([
+          'name'=>'Laptops',
+          'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+      DB::table('categories')->insert([
+          'name'=>'Mobile Phones',
+          'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+
+        ]);
+      DB::table('categories')->insert([
+          'name'=>'Books',
+          'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+      DB::table('categories')->insert([
+          'name'=>'Real Estate',
+          'created_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => \Carbon\Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
