734d5694f60dab17ca78c3de4c42214bb495aaf3
Ahmed Raza <raza1778@gmail.com>
2018-02-22 20:16:57 +0500

    Users dashboard index view

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="container">
+    <h1>Users</h1>
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Total Ads</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($users as $key => $user)
+          <tr>
+            <td>{{ $key+1 }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->role }}</td>
+            <td>{{ count($user->ads) }}</td>
+            <td><a href="#">Edit</a> | <a href="#">Delete</a></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+@stop
