c7c0e3f634097a8e4bfba6aab7d7cc97135421b4
Ahmed Raza <raza1778@gmail.com>
2018-02-22 23:08:43 +0500

    Ads form restructured upload file field

--- resources/views/ads/partials/_form.blade.php
+++ resources/views/ads/partials/_form.blade.php
@@ -30,9 +30,28 @@
   {!! Form::label('description') !!}
   {!! Form::textarea('description', null, ['class'=>'ckeditor']) !!}
 </div>
-<div class="form-group">
-  {!! Form::label('images') !!}
-  {!! Form::file('images', ['class'=>'form-control']) !!}
+<div class="row">
+  <div class="{{ $edit ? 'col-lg-6' : 'col-lg-12' }}">
+    <div class="{{ $edit ? '' : 'form-group' }}">
+      {!! Form::label('images') !!}
+      {!! Form::file('images[]', ['class'=>'form-control', 'multiple'=>true]) !!}
+    </div>
+  </div>
+  @if ($edit && $ad->images)
+  <div class="col-lg-6">
+    <div class="row">
+    @foreach (array_filter(explode(';', $ad->images), 'strlen') as $image)
+      <div class="col-lg-4">
+        <a href="/files/ads/{{ $ad->id }}/{{ $image }}" target="_blank">
+          <img src="/files/ads/{{ $ad->id }}/{{ $image }}" alt="{{ $image }}" height="100" width="100">
+        </a><br>
+        <a href="javascript:void(0)" id="remove-image" class="text-center">Remove</a>
+        {!! Form::hidden('image_names[]', $image) !!}
+      </div>
+    @endforeach
+    </div>
+  </div>
+  @endif
 </div>
 <div class="form-group">
   {!! Form::checkbox('pull_contact_info', 1, $edit ? $ad->pull_contact_info : false, ['id'=>'pull_contact_info']) !!}
@@ -68,5 +87,11 @@
     if(this.checked) {
       $('.custom-contact-info').hide();
     }
+  });
+  $('a#remove-image').each(function() {
+    $(this).click(function(e) {
+      $(this).attr('data-image-name');
+      $(this).parents('.col-lg-4').remove();
+    });
   });
 </script>
