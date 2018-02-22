eacd608026ed727d9c78f82a65f7b5a769961616
Ahmed Raza <raza1778@gmail.com>
2018-02-22 20:17:55 +0500

    Add category button popup modal form

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -4,11 +4,13 @@
 
   <div class="container">
     <h1>Categories</h1>
-    <table class="table table-hovered">
+    <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#categoryModal">Add Category</button>
+    <table class="table table-hover">
       <thead>
         <tr>
           <th>#</th>
           <th>Name</th>
+          <th>Total Ads</th>
           <th>Actions</th>
         </tr>
       </thead>
@@ -17,11 +19,16 @@
           <tr>
             <td>{{ $key+1 }}</td>
             <td>{{ $category->name }}</td>
+            <td>{{ count($category->ads) }}</td>
             <td><a href="#">Edit</a> | <a href="#">Delete</a></td>
           </tr>
         @endforeach
       </tbody>
     </table>
   </div>
+
+{!! Form::open(['url'=>route('category.store'), 'method'=>'POST']) !!}
+  @include('admin.categories.partials._form', ['edit'=>false, 'title'=>'Add Category'])
+{!! Form::close() !!}
 
 @stop
