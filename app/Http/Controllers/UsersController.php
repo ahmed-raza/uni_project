1a9ca684ad94752cea295860300c064f0cc060a6
Ahmed Raza <raza1778@gmail.com>
2018-02-23 17:54:36 +0500

    edit, update actions and middlewares

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,19 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UsersRequest;
 use Auth;
+use App\User;
 
 class UsersController extends Controller
 {
   public function __construct(){
-    $this->middleware('auth', ['only'=>['ads']]);
+    $this->middleware('auth', ['except'=>['profile']]);
+    $this->middleware('userOrAdmin', ['only'=>['ads', 'edit']]);
   }
   public function profile() {
     $user = Auth::user();
     return view('users.profile', compact('user'));
   }
-  public function ads() {
-    $ads = Auth::user()->ads;
-    return view('users.ads', compact('ads'));
+  public function ads($id) {
+    $user = User::findOrFail($id);
+    return view('users.ads', compact('user'));
+  }
+  public function edit($id) {
+    $user = User::findOrFail($id);
+    return view('users.edit', compact('user'));
+  }
+  public function update($id, UsersRequest $request) {
+    $user = User::findOrFail($id);
+    $user->update($request->all());
+    return redirect()->back()->with('message', 'User updated.');
   }
 }
