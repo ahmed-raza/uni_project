952879692e095ad75d03ec364caae3f8a38ac7cf
Ahmed Raza <raza1778@gmail.com>
2018-02-23 16:18:33 +0500

    Admin users index added id

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,7 +7,7 @@
     <table class="table table-hover">
       <thead>
         <tr>
-          <th>#</th>
+          <th>ID</th>
           <th>Name</th>
           <th>Email</th>
           <th>Role</th>
@@ -18,7 +18,7 @@
       <tbody>
         @foreach($users as $key => $user)
           <tr>
-            <td>{{ $key+1 }}</td>
+            <td>{{ $user->id }}</td>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
             <td>{{ $user->role }}</td>
