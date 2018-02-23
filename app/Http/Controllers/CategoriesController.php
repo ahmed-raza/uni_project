551effeacc3064973a3d781915c12617bfd185d3
Ahmed Raza <raza1778@gmail.com>
2018-02-23 16:13:33 +0500

    Delete, Edit actions & Admin middleware except for show

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,6 +7,9 @@ use App\Category;
 
 class CategoriesController extends Controller
 {
+  public function __construct() {
+    $this->middleware('admin', ['except'=>['show']]);
+  }
   public function index() {
     $categories = Category::all();
     return view('admin.categories.index', compact('categories'));
@@ -19,5 +22,14 @@ class CategoriesController extends Controller
     $category = Category::find($id);
     $category->update($request->all());
     return redirect()->back()->with('message', 'Category created.');
+  }
+  public function delete($id) {
+    $category = Category::findOrFail($id);
+    return view('partials.delete')->with(['entity_type'=>'categories', 'entity_id'=>$category->id]);
+  }
+  public function destroy($id) {
+    $category = Category::findOrFail($id);
+    $category->delete();
+    return redirect(route('admin.dashboard'))->with('message', 'Category deleted.');
   }
 }
