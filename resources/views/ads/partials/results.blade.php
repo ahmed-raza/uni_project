a2e9d17707c8420135396f98e58373e7f923fbaa
Ahmed Raza <raza1778@gmail.com>
2018-03-24 00:26:33 +0500

    Images layout changes on ads index

--- resources/views/ads/partials/results.blade.php
+++ resources/views/ads/partials/results.blade.php
@@ -4,11 +4,11 @@
 @foreach($ads as $ad)
 <div class="row ads__row">
   @if ($ad->images)
-    <div class="col-lg-3">
+    <div class="col-lg-4 ad-image">
       <img src="/files/ads/{{ $ad->id }}/{{ explode(';', $ad->images)[0] }}" alt="#" height="140" width="200">
     </div>
   @endif
-  <div class="{{ $ad->images ? 'col-lg-9' : 'col-lg-12' }}">
+  <div class="{{ $ad->images ? 'col-lg-8' : 'col-lg-12' }}">
     <h3>{{ Html::link(route('ads.show',$ad->slug), $ad->title) }}</h3>
     <em>{{ $ad->created_at->diffForHumans() }}</em>
     {!! str_limit($ad->description, 200) !!}
