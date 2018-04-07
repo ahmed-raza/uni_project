b5715534ac1e1e8ee59403eb6040e5ffa584326d
Alex Roye <alex@creativefaze.com>
2018-04-07 23:07:48 +0500

    Profile edit route on user profile

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -4,6 +4,7 @@
 
 <div class="container">
   <h1>Profile</h1>
+  {!! Html::link(route('user.edit', $user->id), 'Edit Profile') !!}
   <p>{{ $user->name }}</p>
   <p>{{ $user->email }}</p>
   <p>{{ $user->role }}</p>
