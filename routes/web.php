38c50e84a3181d8c08f218e56f3f0a9d1a831edc
Alex Roye <alex@creativefaze.com>
2018-04-07 23:07:33 +0500

    Rearranged routes

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,22 @@
 */
 
 Route::get('/', 'PagesController@home')->name('home');
-Route::get('user/profile', 'UsersController@profile')->name('user.profile');
+
+// Ads Routes
 Route::resource('ads', 'AdsController');
 Route::get('ads/{id}/delete', 'AdsController@delete')->name('ads.delete');
+
+// Categories Routes
 Route::resource('categories', 'CategoriesController');
 Route::get('categories/{id}/delete', 'CategoriesController@delete')->name('categories.delete');
+
+// User Routes
 Route::get('user/{id}/edit', 'UsersController@edit')->name('user.edit');
 Route::get('user/{id}/ads', 'UsersController@ads')->name('user.ads');
 Route::patch('user/{id}', 'UsersController@update')->name('user.update');
+Route::get('user/profile', 'UsersController@profile')->name('user.profile');
 
+// Admin Routes
 Route::group(['prefix'=>'admin', 'middleware'=>['admin']], function(){
   Route::get('dashboard', 'AdminController@dashboard')->name('admin.dashboard');
   Route::get('users', 'AdminController@users')->name('admin.users');
