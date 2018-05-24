5bb60a0c01ff3ad3194eae840694c3e2e08db7cb
Alex Roye <alex@creativefaze.com>
2018-05-24 18:17:40 +0500

    Reminder mail template

---
+++ resources/views/emails/reminder.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <style>
+    h1 {
+      color: red;
+      text-shadow: 0 0 5px red;
+    }
+  </style>
+</head>
+<body>
+  <h1>Reminder</h1>
+  <p>This is a reminder that you have to check logs on production.</p>
+</body>
+</html>
